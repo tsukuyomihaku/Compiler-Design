1ab7437fa0e8351684c9b088766cfc0f87d37912
tsukuyomihaku <srazb32@gmail.com>
2026-05-18 12:00:17 +0545

    Format lab 1 and add lab 2

---
+++ Lab_1/lab1.s
@@ -0,0 +1,50 @@
+	.file	"lab1.c"
+	.text
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Enter two numbers: \0"
+.LC1:
+	.ascii "%d %d\0"
+.LC2:
+	.ascii "Sum = %d\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rcx
+	call	printf
+	leaq	-12(%rbp), %rdx
+	leaq	-8(%rbp), %rax
+	movq	%rdx, %r8
+	movq	%rax, %rdx
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rcx
+	call	scanf
+	movl	-8(%rbp), %edx
+	movl	-12(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %edx
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (Rev6, Built by MSYS2 project) 13.1.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
+	.def	scanf;	.scl	2;	.type	32;	.endef
